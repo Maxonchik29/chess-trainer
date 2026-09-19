3159cecaa483fca458bed19324e83266be0c290a
Maxonchik29 <maxmaxrudenko29@gmail.com>
2026-09-20 01:09:33 +0300

    Speed up mistake training computer

--- web_server.py
+++ web_server.py
@@ -1247,7 +1247,7 @@ def mistake_computer_move():
             result = engine.analyse(
                 board,
                 chess.engine.Limit(
-                    depth=12
+                    time=1.0
                 )
             )
 
